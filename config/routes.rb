eb78980a6db5c7a4e1fd98b9fd3263e29ba47dbd
Fernando Seguim <fernando.seguim@gmail.com>
2017-05-25 16:03:20 -0300

    generate produtos

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "produtos" => "produtos#index"
+  get "todos" => "produtos#show_all", as: "todos_os_produtos"
+  root "produtos#index"
 end
